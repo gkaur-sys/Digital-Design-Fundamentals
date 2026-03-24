02401386d7691635f9dacede657cc7f553f55de3
Gunpreet Kaur <gunpreetkaur07@gmail.com>
2026-03-24 20:10:41 +0530

    Added 4:1 multiplexer using behavioral modeling (case-based implementation)

--- combinational/mux_4to1/mux_behavioral.v
+++ combinational/mux_4to1/mux_behavioral.v
@@ -1,1 +1,17 @@
+module mux_behavioral (
+    input a, b, c, d,
+    input [1:0] sel,
+    output reg y
+);
 
+always @(*) begin
+    case(sel)
+        2'b00: y = a;
+        2'b01: y = b;
+        2'b10: y = c;
+        2'b11: y = d;
+        default: y = 1'b0; 
+    endcase
+end
+
+endmodule
