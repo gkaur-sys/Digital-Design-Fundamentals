116e1d684441351e0f3d30d92eb295e090422e2f
Gunpreet Kaur <gunpreetkaur07@gmail.com>
2026-03-24 20:25:09 +0530

    Added basic testbench for 4:1 multiplexer behavioral model

--- combinational/mux_4to1/mux_tb.v
+++ combinational/mux_4to1/mux_tb.v
@@ -1,1 +1,26 @@
+module mux_tb;
+reg a,b,c,d;
+reg [1:0] sel;
+wire y;
+mux_behavorial uut(
+.a(a),
+.b(b),
+.c(c),
+.d(d),
+.sel(sel),
+.y(y)
+);
+
+initial begin 
+a = 0 ; b = 1; c = 0 ; d =1; 
+sel = 2'b00; #10;
+sel = 2'b01; #10;
+sel = 2'b10; #10;
+sel = 2'b11; #10;
+
+$finish;
+end
+ 
+
+endmodule
 
